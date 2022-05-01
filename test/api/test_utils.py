d0bb2ac34f76dfa4e70d558a0a43365953781714
MichaelJAliberti <mjali@bu.edu>
2022-05-01 18:26:11 -0400

    linting, formatting, testing

--- test/api/test_utils.py
+++ test/api/test_utils.py
@@ -1,2 +1,24 @@
 import pytest
 
+from src.api.utils import copy_class_def
+
+
+@pytest.mark.parametrize(
+    "classname, value",
+    [
+        [
+            "Bar",
+            5,
+        ],
+    ],
+)
+def test_copy_class_def(classname, value):
+    class Foo:
+        def __init__(self, val):
+            self.val = val
+
+    NewClass = copy_class_def(name=classname, class_def=Foo)
+    bar = NewClass(value)
+
+    assert type(bar).__name__ == classname
+    assert bar.val == value
